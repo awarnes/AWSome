642a3d61f95eeff9a0c6eba5971db7237d855083
Alex Warnes <warnes.alexander@gmail.com>
2017-10-13 00:00:55 -0700

    The basic server.

---
+++ index.js
@@ -0,0 +1,26 @@
+const newrelic = require('newrelic')
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hey!')
+})
+
+app.get('/long', (req, res) => {
+  const len = 10000000
+  Array(len).fill('').forEach(() => {
+    let x = Math.random()
+  })
+  res.send('This took too long!')
+})
+
+app.get('/break', (req, res) => {
+  const len = 100000000
+  Array(len).fill().forEach(() => {
+    let x = Math.random()
+  })
+  res.send('This didn\'t work!')
+})
+
+app.listen(3000, () => console.log('Server running on port 3000')
+

13b9551205d3528102009963fce67cc358e46e99
Alex Warnes <warnes.alexander@gmail.com>
2017-10-13 01:13:00 -0700

    Full update to Didgeri-Downunder website.

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const newrelic = require('newrelic')
 const express = require('express')
+const fetch = require('node-fetch')
 const app = express()
 
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
-  res.send('Hey!')
+  res.render('index', { title: 'Didgeri-Downunder', message: 'Welcome!'})
 })
 
-app.get('/long', (req, res) => {
-  const len = 10000000
-  Array(len).fill('').forEach(() => {
-    let x = Math.random()
-  })
-  res.send('This took too long!')
+app.get('/store', (req, res) => {
+  const items = [{name: 'biggeridoo', price: 99.99}, {name: 'medium', price: 49.99}, {name: 'smalleridoo', price: 12.99}]
+  res.render('store', {items})
 })
 
-app.get('/break', (req, res) => {
-  const len = 100000000
-  Array(len).fill().forEach(() => {
-    let x = Math.random()
-  })
-  res.send('This didn\'t work!')
+app.get('/ceo', (req, res) => {
+  fetch('https://randomuser.me/api/')
+    .then(resp => resp.json())
+    .then(json => {
+      res.render('people', {person: json.results[0]})      
+    })
 })
 
 app.listen(3000, () => console.log('Server running on port 3000'))
